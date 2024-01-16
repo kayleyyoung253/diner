cca4deb7960edd0f8f9ceeba3f6f4d07ac5e6680
kayley.young253 <kayley.young253@gmail.com>
2024-01-16 12:57:11 -0500

    Added commands to instantiate Fat-Free

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-echo "My Diner";
+/*
+ * Kayley Young
+ * January 2024
+ * https://kyoung38.greenriverdev.com/328/diner/
+ * This is my CONTROLLER for the Diner app
+ */
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once ('vendor/autoload.php');
+
+//Instantiate Fat-Free framework (f3)
+$f3 = Base::instance();
+
+//Define a default route-invoking route method
+$f3->route('GET /', function() {
+    echo "My Diner";
+});
+// Run Fat-Free
+$f3->run();
+
 
